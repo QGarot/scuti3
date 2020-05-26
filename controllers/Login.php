19be09965c6881c395ae27bbd53b9d8e195ed1ca
#Tig3r <47186964+Tiig3r@users.noreply.github.com>
2020-05-26 14:53:44 +0200

    Update controllers (start login system)

--- controllers/Login.php
+++ controllers/Login.php
@@ -1,2 +1,24 @@
 <?php
+/*
+    TODO: check if user is banned
+*/
+if(isset($_POST["login-username"], $_POST["login-password"])) {
+    if(!empty($_POST["login-username"]) AND !empty($_POST["login-password"])) {
+        $loginUsername = htmlspecialchars($_POST["login-username"]);
+        if(userExist($loginUsername)) {
+            if(password_verify($_POST["login-password"], getPasswordCrypted($loginUsername))) {
+                $userInfo = getUserByUsername($loginUsername);
+                $_SESSION["id"] = $userInfo["id"];
+                header("Location: ?action=me");
+            } else {
+                $error = "Données incorrectes !";
+            }
+        } else {
+            $error = "Ce compte n'existe pas !";
+        }
+    } else {
+        $error = "Veuillez remplir tous les champs !";
+    }
+}
+
 require("views/index.php");
--- controllers/Profile.php
+++ controllers/Profile.php
@@ -1,3 +1,8 @@
 <?php
+if(!isset($_SESSION["id"])) {
+    header("Location: index.php");
+}
+
+$userInfo = getUserById($_SESSION["id"]);
 $reqRooms = getAllRooms();
 require("views/me.php");
