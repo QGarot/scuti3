2dbb2dd47b1c46bf0c432439d548ce600abdf335
#Tig3r <47186964+Tiig3r@users.noreply.github.com>
2020-06-14 00:54:33 +0200

    Update footer

--- public/template/footer.php
+++ public/template/footer.php
@@ -1,8 +1,8 @@
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <div style="margin-top: 20px; padding: 10px; margin-bottom: 20px; background-color: #ffffff; float: left; width: 100%; border-radius: 5px;">
-                <img src="public/assets/img/me_credits_active.gif" /><span style="margin-left: 10px;">© <strong>Scuti3</strong> | Développé par <strong>Tig3r</strong></span>
+<div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div style="margin-top: 20px;padding: 10px;margin-bottom: 20px;background-color: #ffffff;float: left;width: 100%;border-radius: 5px;"><img src="public/assets/img/me_credits_active.gif"><span style="margin-left: 10px;">© <strong>Scuti3</strong> | Développé avec <i class="fa fa-heart" style="color: rgb(238,82,72);"></i>&nbsp;par <strong>Tig3r</strong></span></div>
             </div>
         </div>
     </div>
